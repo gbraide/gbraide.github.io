69ebe1f441fea21ef6fb2a002e2177d3479ed074
Grace <g7@Graces-MBP.fios-router.home>
2017-11-06 04:25:15 -0500

    Clean up styling

--- src/app/project-list.ts
+++ src/app/project-list.ts
@@ -4,17 +4,17 @@ export const PROJECTS: Project[] = [
  { 
      id: 1, 
      name: 'React', 
-     description: 'A bookshelf app that allows you to select and categorize books you have read, are currently reading, or want to read', 
+     description: 'A bookshelf app that allows a user to select and categorize books they have read, are currently reading, or want to read', 
      link: 'github.com/gbraide/project-myreads' },
  { 
      id: 2, 
      name: 'React w/ Redux', 
-     description: "A content and comment web app.  Users are able to post content to predefined categories, comment on their posts and other users' posts, and vote on posts and comments. Users are also able to edit and delete posts and comments", 
+     description: "A content and comment web app that allows users to post content to predefined categories, comment on their posts and other users' posts, and vote on posts and comments. Users are also able to edit and delete posts and comments", 
      link: 'github.com/gbraide/project-readable' 
 },
  { 
      id: 3, 
      name: 'Angular', 
-     description: 'A portfolio site that displays recent projects', 
+     description: 'A simple portfolio site that displays reusable components, routing, and services, with Angular.', 
      link: 'github.com/gbraide/gbraide.github.io/tree/dev' }
 ];
